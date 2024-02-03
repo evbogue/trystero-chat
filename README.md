0914ceac00367a10b70e079650b5a34ed208b3f8
Ev Bogue <ev@evbogue.com>
2024-02-03 12:46:11 -0600

    initial

---
+++ README.md
@@ -0,0 +1,12 @@
+# Trying Trystero
+
+A chat client built with [Trystero-torrent](https://github.com/dmotz/trystero) 
+
+in Deno
+
+```
+deno run --allow-all serve.js
+```
+
+---
+MIT
---
+++ app.js
@@ -0,0 +1,31 @@
+import { joinRoom } from './trystero-torrent.min.js'
+import { h } from './h.js'
+
+const input = h('input')
+
+const screen = h('div', [
+  input,
+  h('button', {onclick: () => {
+    if (input.value) {
+      sendmsg({msg: input.value})
+      screen.appendChild()
+    }
+  }}, ['Send'])
+])
+
+document.body.appendChild(screen)
+
+const config = {appId: 'testing4321'}
+
+const room = joinRoom(config, 'testroom')
+
+const [ sendmsg, onmsg ] = room.makeAction('message')
+
+room.onPeerJoin(peerId => screen.appendChild(h('div', [peerId + ' joined.'])))
+
+room.onPeerLeave(peerId => screen.appendChild(h('div', [peerId + ' left.'])))
+
+onmsg((data, peerId) => {
+  screen.appendChild(h('div', [peerId + ': ' + data.msg])) 
+}) 
+
